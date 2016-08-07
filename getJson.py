1397932b942081283cc2d9032de3b72179fbdf16
Lydoo <bo_bo0425@126.com>
2016-08-07 23:31:13 +0800

    handler request

--- getJson.py
+++ getJson.py
@@ -12,7 +12,7 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import BaseHTTPServer
 import io, shutil
-import urllib
+import urllib, urllib2
 import os, sys, json
 class MyRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -22,9 +22,7 @@ class MyRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         mpath,margs=urllib.splitquery(self.path)
         datas = self.rfile.read(int(self.headers['content-length']))
-        self.do_action(mpath, datas)
-        dic = {"status":200, "group":8}
-        str = json.dumps(dic)
+        str = self.do_action(mpath, datas)
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Content-type','application/json')
@@ -33,10 +31,39 @@ class MyRequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(str)
         self.connection.shutdown(1)
 
+
     def do_action(self, path, args):
         print path
-
         print type(args), args
+        JNstr = json.loads(args)
+        if JNstr['Priority'] == 8:
+            dic = {"status":200, "group":8}
+            return json.dumps(dic)
+        else:
+            dic = {
+            	'JobID': 'John',
+            	'Priority': 8,
+            	'Description':'c://json.json',
+            	'TestOutput': {
+                  "order_url":'',
+                  "response":'',
+                  "response1":'',
+                  "response2":'',
+                  "response3":'',
+                  "response4":[1,2,3,1123,123123],
+                },
+            	'ExpectOutput': 28,
+            	'status':'waiting',
+            }
+            return json.dumps(dic)
+
+    def send_data(self):
+        url = "http://localhost:8000/TestCaseEditor/html/TestReport.html"
+        data ={'user':'Smith','passwd':'123456'}
+        jsonStr = json.dumps(data)
+        req = urllib2.Request(url, jsonStr)
+        responses = urllib2.urlopen(req)
+        return responses.read()
 
 server_address = ('127.0.0.1', 8888)
 server_class=BaseHTTPServer.HTTPServer
